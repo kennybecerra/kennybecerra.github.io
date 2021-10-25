a1533b8ab301715761fb05dd321f450601255822
Kenny Becerra <KennyBecerra.j@gmail.com>
2021-10-24 22:11:06 -0700

    added .env file, updated deploy script to use .env file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,19 @@
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 import Client from 'ssh2-sftp-client';
+
+dotenv.config();
 
 const sftp = new Client();
 
 const config = {
-  host: 'kennybecerra.com',
-  username: 'kenbec8',
-  privateKey: fs.readFileSync('C:\\Users\\kenny\\.ssh\\dreamhost_website', 'utf-8'),
+  host: process.env.WEBSITE_HOST,
+  username: process.env.WEBSITE_USERNAME,
+  privateKey: fs.readFileSync(process.env.WEBSITE_RSA_LOCATION, 'utf-8'),
 };
 
-const siteDir = 'kennybecerra.com';
+const siteDir = process.env.WEBSITE_REMOTE_DIRECTORY;
 const fileToBedeleted = new RegExp('\\.(js|ico|css|html|gif)$');
 const dirToBeDeleted = new RegExp('(assets|js|css)');
 
@@ -18,7 +21,7 @@ const deployCode = async () => {
   try {
     await sftp.connect(config);
 
-    let list = await sftp.list(siteDir).then((res) => res.filter(({ name }) => !name?.startsWith('.')));
+    const list = await sftp.list(siteDir).then((res) => res.filter(({ name }) => !name?.startsWith('.')));
 
     const result = await Promise.allSettled(
       list.map(({ type, name }) => {
